d7dcb71685b1067aff851c1a1be52975fa5cf1a5
noorabdullah02 <sheikhnoorabdullah02@gmail.com>
2025-10-03 12:19:08 +0600

    File Added

---
+++ JavaScript/Day1/index.js
@@ -0,0 +1,28 @@
+let name ='Rohit';
+let age=10;
+console.log(name,age);
+
+age=15;
+console.log(name,age);
+
+let imu;
+console.log(imu);
+
+let arr = [1,2,3,4,5,'Rohit',true];
+
+console.log(arr);
+
+
+
+//let score = 30
+
+//console.log(typeof score) 
+
+//console.log(-true)
+
+console.log(2<3)
+
+let a = 10
+let b = 20
+
+console.log(`a is less theb ${a} ${b}`)
