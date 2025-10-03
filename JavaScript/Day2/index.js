3f1f6c83496eb4774657193d892d9821cc52fa66
noorabdullah02 <sheikhnoorabdullah02@gmail.com>
2025-10-03 12:55:47 +0600

    Added

--- JavaScript/Day2/index.js
+++ JavaScript/Day2/index.js
@@ -1,9 +1,32 @@
 
 
-const score = 400
-console.log(score)
+// const score = 400
+// console.log(score)
 
-const balance = new Number(100)
-console.log(balance)
+// const balance = new Number(100)
+// console.log(balance)
 
-console.log(balance.toFixed(2)) 
+// console.log(balance.toFixed(2)) 
+
+
+const arr = new Array(1,2,3,4,5,'Rohit',true);
+
+//console.log(arr);
+
+// arr.push(6)
+
+// console.log(arr);
+
+// arr.pop()
+
+//arr.unshift(0)
+
+console.log(arr);
+
+arr.shift()
+
+console.log(arr);
+
+const newArr = arr.join();
+
+console.log( typeof newArr);
